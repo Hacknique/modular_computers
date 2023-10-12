801ba9a229d65618fd6e1d1e297983700eb9d977
James David Clarke <james@jamesdavidclarke.com>
2023-10-12 05:59:59 +0100

    Added formspec for terminal

--- ctr_nodes/src/computer_node.lua
+++ ctr_nodes/src/computer_node.lua
@@ -8,7 +8,15 @@
 -- Copyright (c) 2023 James Clarke <james@jamesdavidclarke.com>
 -- Copyright (c) 2023 nitrogenez
 
+local terminal_text = ""
 
+function formspec(terminal_text)
+return "size[15,10]" .. 
+    "textarea[0.5,0.5;14,8;terminal_out;Terminal:;" .. minetest.formspec_escape(terminal_text) .. "]" .. 
+    "field_close_on_enter[terminal_in;false]" .. 
+    "set_focus[terminal_in;true]" ..
+    "field[0.5,9;14,1;terminal_in;Input Command:;]"
+end
 -- register the computer node
 minetest.register_node("ctr_nodes:computer", {
     description = ctr.S("Computer"),
@@ -25,7 +33,29 @@ minetest.register_node("ctr_nodes:computer", {
     light_source = 6,
     paramtype2 = "facedir",          -- needed for the node to rotate properly on place
     on_place = minetest.rotate_node, -- rotates the node on place
+    on_rightclick = function(pos, node, player)
+        local formname = "ctr_nodes:computer_formspec"
+        minetest.show_formspec(player:get_player_name(), formname, formspec(""))
+    end,
 })
+
+-- Handle form submission
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+    if formname == "ctr_nodes:computer_formspec" then
+        if fields.key_enter_field == "terminal_in" then
+            -- Append the command to the terminal text
+            if fields.terminal_out ~= "" then
+                terminal_text = fields.terminal_out .. "\n" .. fields.terminal_in
+            else
+                terminal_text = fields.terminal_in
+            end
+            minetest.log("action", "[ctr]:\t" .. "Player:\t" .. player:get_player_name() .. "Submitted command:\t" .. fields.terminal_in)
+            -- Show the updated formspec to the player
+            minetest.show_formspec(player:get_player_name(), formname, formspec(terminal_text))
+        end
+    end
+end)
+
 
 -- local shortcut for get_modpath
 local modpath = minetest.get_modpath
