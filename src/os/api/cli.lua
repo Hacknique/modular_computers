b3ad6e2342d27178850a2d68d2227e2c8d479802
y5nw <37980625+y5nw@users.noreply.github.com>
2023-10-15 18:22:35 +0200

    command.register_command -> command.register as requested on Matrix

--- src/os/api/cli.lua
+++ src/os/api/cli.lua
@@ -1,6 +1,6 @@
 modular_computers.command = {}
 modular_computers.internal.command = {registered_commands = {}}
 
-function modular_computers.command.register_command(name, callback)
+function modular_computers.command.register(name, callback)
     modular_computers.internal.command.registered_commands[name] = callback
 end
--- src/os/bin/echo.lua
+++ src/os/bin/echo.lua
@@ -1,4 +1,4 @@
-modular_computers.command.register_command("echo", {
+modular_computers.command.register("echo", {
     func = function(player, argc, ...)
         return "", table.concat({...}, " ") .. "\n", "", 0
     end,
